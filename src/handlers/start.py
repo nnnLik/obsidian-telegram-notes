d4770769fcfe87dbc44e5de398bfab03f4058878
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-08 16:57:22 +0300

    [ADD] start handler

---
+++ src/handlers/start.py
@@ -0,0 +1,24 @@
+import logging
+
+from aiogram import types
+
+from src.models.account import Account
+from src.services.database import GinoConnection
+from src.utils.config import POSTGRES_URL
+
+logger = logging.getLogger(__name__)
+
+
+async def cmd_start(message: types.Message):
+    user_tg_id = message.from_user.id
+
+    async with GinoConnection(POSTGRES_URL):
+        account = await Account.query.where(Account.tg_id == user_tg_id).gino.first()
+        if account:
+            logger.warning("Account already exists in the database.")
+        else:
+            new_account = Account(tg_id=user_tg_id)
+            await new_account.create()
+            logger.warning("Account created.")
+
+    await message.reply("Hello")
