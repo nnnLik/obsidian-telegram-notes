44cae9bda636f2969ef06761ed66df8d3ef766dc
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-11 13:55:11 +0300

    [ADD] messages and handlers

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,16 @@
+import json
+from typing import Dict
 from decouple import config
 
 
 API_TOKEN = config("API_TOKEN")
 POSTGRES_URL = config("POSTGRES_URL")
+
+
+def get_base_messages() -> Dict[str, str]:
+    with open("src/utils/msg/base.json", "r") as f:
+        msgs: Dict[str, str] = json.load(f)
+        return msgs
+
+
+MESSAGES = get_base_messages()
